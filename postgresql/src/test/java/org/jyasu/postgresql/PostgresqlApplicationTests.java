ca68ff0642689e03b973bbea74c103253bd7618b
jyasuu <s1002605@pu.edu.tw>
2025-01-14 12:15:29 +0000

    try mock data test

--- postgresql/src/test/java/org/jyasu/postgresql/PostgresqlApplicationTests.java
+++ postgresql/src/test/java/org/jyasu/postgresql/PostgresqlApplicationTests.java
@@ -1,13 +1,68 @@
 package org.jyasu.postgresql;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
-@SpringBootTest
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.jyasu.postgresql.account.entity.Account;
+import org.jyasu.postgresql.account.repository.AccountRepository;
+import org.jyasu.postgresql.service.BankService;
+import org.jyasu.postgresql.transaction.repository.TransactionRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class PostgresqlApplicationTests {
+
+    @InjectMocks
+	private BankService bankService;
+
+	
+    @Mock
+    private AccountRepository accountRepository;
+	
+    @Mock
+    private TransactionRepository transactionRepository;
+
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void success() {
+		Account mockAccount = new Account();
+		mockAccount.setId(1l);
+		mockAccount.setVersion(1);
+		mockAccount.setBalance(BigDecimal.valueOf(1000l));;
+		Mockito.when(accountRepository.save(any())).thenReturn(mockAccount);
+		Account account = bankService.createAccount(0, null);
+
+		assertEquals(1l, account.getId());
+		assertEquals(1, account.getVersion());
+		assertEquals(BigDecimal.valueOf(1000l), account.getBalance());
+		
+	}
+	@Test
+	void fail() {
+		Account mockAccount = new Account();
+		mockAccount.setId(1l);
+		mockAccount.setVersion(1);
+		mockAccount.setBalance(BigDecimal.valueOf(1000l));;
+		Mockito.when(accountRepository.save(any())).thenReturn(mockAccount);
+		Account account = bankService.createAccount(0, null);
+
+		assertEquals(1l, account.getId());
+		assertEquals(1, account.getVersion());
+		assertEquals(BigDecimal.valueOf(1010l), account.getBalance());
+		
+	}
+
+	
 
 }
